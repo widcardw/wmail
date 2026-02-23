e9acac076b9c81a1b83c1f1172225c90f6796145
widcardw <lui747353367@qq.com>
2026-02-23 10:47:57 +0800

    use icon for menubar

--- frontend/src/components/ui/editor/menubar.tsx
+++ frontend/src/components/ui/editor/menubar.tsx
@@ -30,8 +30,6 @@ const EditorMenubar: Component<{ editor?: Editor }> = (props) => {
       isHeading2: editor?.isActive('heading', { level: 2 }),
       isHeading3: editor?.isActive('heading', { level: 3 }),
       isHeading4: editor?.isActive('heading', { level: 4 }),
-      isHeading5: editor?.isActive('heading', { level: 5 }),
-      isHeading6: editor?.isActive('heading', { level: 6 }),
       isBulletList: editor?.isActive('bulletList'),
       isOrderedList: editor?.isActive('orderedList'),
       isCodeBlock: editor?.isActive('codeBlock'),
@@ -42,158 +40,157 @@ const EditorMenubar: Component<{ editor?: Editor }> = (props) => {
   )
 
   return (
-    <div class="control-group">
-      <div class="button-group">
-        <Toggle.Root
-          pressed={state().isBold}
-          onPressedChange={() => props.editor!.chain().focus().toggleBold().run()}
-          disabled={!state().canBold}
-          class={clsx(toggleStyles.Root)}
-        >
-          <div class="i-ri-bold"></div>
-        </Toggle.Root>
-        <Toggle.Root
-          pressed={state().isItalic}
-          onPressedChange={() => props.editor!.chain().focus().toggleItalic().run()}
-          disabled={!state().canItalic}
-          class={clsx(toggleStyles.Root)}
-        >
-          <div class="i-ri-italic"></div>
-        </Toggle.Root>
-        <Toggle.Root
-          pressed={state().isStrike}
-          onPressedChange={() => props.editor!.chain().focus().toggleStrike().run()}
-          disabled={!state().canStrike}
-          class={clsx(toggleStyles.Root)}
-        >
-          <div class="i-ri-strikethrough"></div>
-        </Toggle.Root>
-        <Toggle.Root
-          pressed={state().isCode}
-          onPressedChange={() => props.editor!.chain().focus().toggleCode().run()}
-          disabled={!state().canCode}
-          class={clsx(toggleStyles.Root, state().isCode && 'is-active')}
-        >
-          <div class="i-ri-code-s-slash-line"></div>
-        </Toggle.Root>
+    <div
+      class="control-group h-3rem flex items-center gap-1 px-1"
+      style={{ 'border-bottom': '1px solid var(--color-border)' }}
+    >
+      <Toggle.Root
+        pressed={state().isBold}
+        onPressedChange={() => props.editor!.chain().focus().toggleBold().run()}
+        disabled={!state().canBold}
+        class={clsx(toggleStyles.Root)}
+      >
+        <div class="i-ri-bold w-5 h-5"></div>
+      </Toggle.Root>
+      <Toggle.Root
+        pressed={state().isItalic}
+        onPressedChange={() => props.editor!.chain().focus().toggleItalic().run()}
+        disabled={!state().canItalic}
+        class={clsx(toggleStyles.Root)}
+      >
+        <div class="i-ri-italic w-5 h-5"></div>
+      </Toggle.Root>
+      <Toggle.Root
+        pressed={state().isStrike}
+        onPressedChange={() => props.editor!.chain().focus().toggleStrike().run()}
+        disabled={!state().canStrike}
+        class={clsx(toggleStyles.Root)}
+      >
+        <div class="i-ri-strikethrough w-5 h-5"></div>
+      </Toggle.Root>
+      <Toggle.Root
+        pressed={state().isCode}
+        onPressedChange={() => props.editor!.chain().focus().toggleCode().run()}
+        disabled={!state().canCode}
+        class={clsx(toggleStyles.Root, state().isCode && 'is-active')}
+      >
+        <div class="i-ri-code-s-slash-line w-5 h-5"></div>
+      </Toggle.Root>
 
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().unsetAllMarks().run()}
-        >
-          <div class="i-ri-eraser-line" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().clearNodes().run()}
-        >
-          <div class="i-ri-eraser-fill" />
-        </Button>
-        <button
-          onClick={() => props.editor!.chain().focus().setParagraph().run()}
-          class={clsx(state().isParagraph && 'is-active')}
-        >
-          Paragraph
-        </button>
-        <button
-          onClick={() => props.editor!.chain().focus().toggleHeading({ level: 1 }).run()}
-          class={clsx(state().isHeading1 && 'is-active')}
-        >
-          H1
-        </button>
-        <button
-          onClick={() => props.editor!.chain().focus().toggleHeading({ level: 2 }).run()}
-          class={clsx(state().isHeading2 && 'is-active')}
-        >
-          H2
-        </button>
-        <button
-          onClick={() => props.editor!.chain().focus().toggleHeading({ level: 3 }).run()}
-          class={clsx(state().isHeading3 && 'is-active')}
-        >
-          H3
-        </button>
-        <button
-          onClick={() => props.editor!.chain().focus().toggleHeading({ level: 4 }).run()}
-          class={clsx(state().isHeading4 && 'is-active')}
-        >
-          H4
-        </button>
-        <button
-          onClick={() => props.editor!.chain().focus().toggleHeading({ level: 5 }).run()}
-          class={clsx(state().isHeading5 && 'is-active')}
-        >
-          H5
-        </button>
-        <button
-          onClick={() => props.editor!.chain().focus().toggleHeading({ level: 6 }).run()}
-          class={clsx(state().isHeading6 && 'is-active')}
-        >
-          H6
-        </button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().toggleBulletList().run()}
-          class={clsx(state().isBulletList && 'is-active')}
-        >
-          <div class="i-ri-list-check" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().toggleOrderedList().run()}
-          class={clsx(state().isOrderedList && 'is-active')}
-        >
-          <div class="i-ri-list-ordered-2" />
-        </Button>
-        <Toggle.Root
-          pressed={state().isCodeBlock}
-          onPressedChange={() => props.editor!.chain().focus().toggleCodeBlock().run()}
-          class={toggleStyles.Root}
-        >
-          <div class="i-ri-code-block" />
-        </Toggle.Root>
-        <Toggle.Root
-          pressed={state().isBlockquote}
-          onPressedChange={() => props.editor!.chain().focus().toggleBlockquote().run()}
-          class={toggleStyles.Root}
-        >
-          <div class="i-ri-double-quotes-l" />
-        </Toggle.Root>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().setHorizontalRule().run()}
-        >
-          Hr
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().setHardBreak().run()}
-        >
-          <div class="i-ri-arrow-left-down-long-line" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().undo().run()}
-          disabled={!state().canUndo}
-        >
-          <div class="i-ri-arrow-go-back-fill" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => props.editor!.chain().focus().redo().run()}
-          disabled={!state().canRedo}
-        >
-          <div class="i-ri-arrow-go-forward-fill" />
-        </Button>
-      </div>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().unsetAllMarks().run()}
+      >
+        <div class="i-ri-eraser-line w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().setParagraph().run()}
+        class={clsx(state().isParagraph && 'is-active')}
+      >
+        <div class="i-ri-paragraph w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().toggleHeading({ level: 1 }).run()}
+        class={clsx(state().isHeading1 && 'is-active')}
+      >
+        <div class="i-lucide-heading-1 w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().toggleHeading({ level: 2 }).run()}
+        class={clsx(state().isHeading2 && 'is-active')}
+      >
+        <div class="i-lucide-heading-2 w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().toggleHeading({ level: 3 }).run()}
+        class={clsx(state().isHeading3 && 'is-active')}
+      >
+        <div class="i-lucide-heading-3 w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().toggleHeading({ level: 4 }).run()}
+        class={clsx(state().isHeading4 && 'is-active')}
+      >
+        <div class="i-lucide-heading-4 w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().toggleBulletList().run()}
+        class={clsx(state().isBulletList && 'is-active')}
+      >
+        <div class="i-ri-list-check w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().toggleOrderedList().run()}
+        class={clsx(state().isOrderedList && 'is-active')}
+      >
+        <div class="i-ri-list-ordered-2 w-5 h-5" />
+      </Button>
+      <Toggle.Root
+        pressed={state().isCodeBlock}
+        onPressedChange={() => props.editor!.chain().focus().toggleCodeBlock().run()}
+        class={toggleStyles.Root}
+      >
+        <div class="i-ri-code-block w-5 h-5" />
+      </Toggle.Root>
+      <Toggle.Root
+        pressed={state().isBlockquote}
+        onPressedChange={() => props.editor!.chain().focus().toggleBlockquote().run()}
+        class={toggleStyles.Root}
+      >
+        <div class="i-ri-double-quotes-l w-5 h-5" />
+      </Toggle.Root>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().clearNodes().run()}
+      >
+        <div class="i-ri-eraser-fill w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().setHorizontalRule().run()}
+      >
+        <div class="i-lucide-separator-horizontal w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().setHardBreak().run()}
+      >
+        <div class="i-ri-arrow-left-down-long-line w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().undo().run()}
+        disabled={!state().canUndo}
+      >
+        <div class="i-ri-arrow-go-back-fill w-5 h-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => props.editor!.chain().focus().redo().run()}
+        disabled={!state().canRedo}
+      >
+        <div class="i-ri-arrow-go-forward-fill w-5 h-5" />
+      </Button>
     </div>
   )
 }
